5877ce85d8ba70dd60e9898f8ecb9d3be4bf1e85
swhustla <fdkelly@gmail.com>
2023-01-09 13:03:56 +0000

    Add MAPE to metrics module

--- measurements/get_metrics.py
+++ measurements/get_metrics.py
@@ -5,25 +5,45 @@ from methods.get_metrics import get_metrics as method
 
 from predictions.Prediction import PredictionData
 
-#TODO: Add time taken to metrics
+# TODO: Add time taken to metrics
 
-def __get_root_mean_squared_error(ground_truth: np.ndarray, prediction: np.ndarray) -> float:
+
+def __get_root_mean_squared_error(
+    ground_truth: np.ndarray, prediction: np.ndarray
+) -> float:
     return np.sqrt(np.mean((ground_truth - prediction) ** 2))
 
-def __get_r_squared(ground_truth: np.ndarray, prediction: np.ndarray) -> float:
-    return 1 - (np.sum((ground_truth - prediction) ** 2) / np.sum((ground_truth - np.mean(ground_truth)) ** 2))
 
-def __get_mean_absolute_error(ground_truth: np.ndarray, prediction: np.ndarray) -> float:
+def __get_r_squared(ground_truth: np.ndarray, prediction: np.ndarray) -> float:
+    return 1 - (
+        np.sum((ground_truth - prediction) ** 2)
+        / np.sum((ground_truth - np.mean(ground_truth)) ** 2)
+    )
+
+
+def __get_mean_absolute_error(
+    ground_truth: np.ndarray, prediction: np.ndarray
+) -> float:
     return np.mean(np.abs(ground_truth - prediction))
 
+
+def __get_mean_absolute_percentage_error(
+    ground_truth: np.ndarray, prediction: np.ndarray
+) -> float:
+    return np.mean(np.abs((ground_truth - prediction) / ground_truth)) * 100
+
+
 __dict_of_metrics = {
-        "root_mean_squared_error": __get_root_mean_squared_error,
-        "r_squared": __get_r_squared,
-        "mean_absolute_error": __get_mean_absolute_error,
-    }
+    "root_mean_squared_error": __get_root_mean_squared_error,
+    "r_squared": __get_r_squared,
+    "mean_absolute_error": __get_mean_absolute_error,
+    "mean_absolute_percentage_error": __get_mean_absolute_percentage_error,
+}
+
 
 def __round_to_2dp(value: float) -> float:
     return round(value, 2)
+
 
 def __metrics(prediction: PredictionData) -> Dict[str, float]:
     if type(prediction.ground_truth_values) == np.ndarray:
@@ -34,7 +54,15 @@ def __metrics(prediction: PredictionData) -> Dict[str, float]:
         prediction = prediction.values[prediction.prediction_column_name].values
     else:
         prediction = prediction.values
-    return dict(map(lambda metric: (metric, __round_to_2dp(__dict_of_metrics[metric](ground_truth, prediction))), __dict_of_metrics))
+    return dict(
+        map(
+            lambda metric: (
+                metric,
+                __round_to_2dp(__dict_of_metrics[metric](ground_truth, prediction)),
+            ),
+            __dict_of_metrics,
+        )
+    )
 
 
 get_metrics = method(__metrics)
