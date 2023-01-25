0e437d426f2ef96b55bd907543a2bd57f84fc304
swhustla <fdkelly@gmail.com>
2023-01-25 11:09:55 +0000

    Formatting

--- methods/tsetlin_machine.py
+++ methods/tsetlin_machine.py
@@ -18,25 +18,28 @@ Data = TypeVar("Data", contravariant=True)
 Model = TypeVar("Model")
 Prediction = TypeVar("Prediction")
 
+
 def tsetlin_machine(
     seasonal_decompose_data: Callable[[Dataset], Dataset],
     get_best_model_config: Callable[[Dataset], Tuple[Dict, int]],
     get_forecast: Callable[[Dataset, dict], Prediction],
-    combine_trend_seasonal_residual: Callable[[Prediction, Prediction, Prediction, Data], Prediction],
+    combine_trend_seasonal_residual: Callable[
+        [Prediction, Prediction, Prediction, Data], Prediction
+    ],
 ) -> Predict[Dataset, PredictionData]:
     """
     Return a function that takes a dataset and returns a prediction.
     """
+
     def predict(dataset: Dataset, parallel: bool = False) -> PredictionData:
         """
         Return a prediction for the given dataset.
         In this case, the prediction is a combination of the trend, seasonal and residual predictions.
-        
+
         """
         logging.info("Tsetlin Machine Regression")
         # make a copy of the dataset by iterating through the object
         dataset = Dataset(**dataset.__dict__)
-
 
         # decompose the data into trend, seasonal and residual components
         stl_dataset = seasonal_decompose_data(dataset)
@@ -46,20 +49,28 @@ def tsetlin_machine(
 
         # print out the three components in three columns for easy comparison
         input_data_components = pd.concat(
-        [trend_dataset_values, seasonal_dataset_values, residual_dataset_values], axis=1
+            [trend_dataset_values, seasonal_dataset_values, residual_dataset_values],
+            axis=1,
         )
         input_data_components.columns = ["trend", "seasonal", "residual"]
         logging.info(f"Three input components:\n{input_data_components.head()}")
 
         list_of_predictions = []
-        for dataset_component in [trend_dataset_values, seasonal_dataset_values, residual_dataset_values]:
+        for dataset_component in [
+            trend_dataset_values,
+            seasonal_dataset_values,
+            residual_dataset_values,
+        ]:
             dataset.values = dataset_component
             logging.info(f"Running Tsetlin for stream: {dataset_component.name}")
-            params, number_of_iterations = get_best_model_config(dataset, parallel)   
-            list_of_predictions.append(get_forecast(dataset, params, number_of_iterations))
+            params, number_of_iterations = get_best_model_config(dataset, parallel)
+            list_of_predictions.append(
+                get_forecast(dataset, params, number_of_iterations)
+            )
         return combine_trend_seasonal_residual(*list_of_predictions, stl_dataset)
-            
+
     return predict
+
 
 def tsetlin_machine_single(
     get_best_model_config: Callable[[Dataset], Tuple[Dict, int]],
@@ -68,15 +79,13 @@ def tsetlin_machine_single(
     """
     Return a function that takes a dataset and returns a prediction.
     """
+
     def predict(dataset: Dataset, parallel: bool = False) -> PredictionData:
         """
         Return a prediction for the given dataset.
         """
         logging.info("Tsetlin Machine Regression")
-        params, number_iterations = get_best_model_config(dataset, parallel)
+        params, number_of_iterations = get_best_model_config(dataset, parallel)
         return get_forecast(dataset, params, number_of_iterations)
+
     return predict
-
-
-
-
