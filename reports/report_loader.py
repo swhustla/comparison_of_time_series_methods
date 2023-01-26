f115b040394891b511e057616470164b31e5790a
swhustla <fdkelly@gmail.com>
2023-01-26 12:32:09 +0000

    Correction to date format when loading

--- reports/report_loader.py
+++ reports/report_loader.py
@@ -14,7 +14,7 @@ Data = TypeVar("Data", contravariant=True)
 def __file_to_dataframe(file: Path) -> pd.DataFrame:
     """Method to load in the reports from the reports folder."""
     report_df = pd.read_csv(file, index_col=0)
-    report_df["Start Time"] = pd.to_datetime(report_df["Start Time"],dayfirst=True)
+    report_df.index = pd.to_datetime(report_df.index, dayfirst=True)
     return report_df
 
 
