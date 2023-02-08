713c73d90e9dcdd10d90323574f86c7adfaedc9b
swhustla <fdkelly@gmail.com>
2023-02-08 10:23:19 +0000

    Add loader from JSON to PredictionData object

--- reports/report_loader.py
+++ reports/report_loader.py
@@ -3,15 +3,42 @@
 from pathlib import Path
 from typing import Generator, List, Optional, Tuple, Generic, TypeVar, Protocol
 
+import gzip
+
 import pandas as pd
 
 from methods.report_loader import report_loader as method
+from predictions.Prediction import PredictionData
 
 Report = TypeVar("Report", contravariant=True)
 Data = TypeVar("Data", contravariant=True)
 
 
-def __file_to_dataframe(file: Path) -> pd.DataFrame:
+import json
+import gzip
+import pandas as pd
+from pathlib import Path
+
+
+
+def __zipped_json_file_to_prediction_data_object(file: Path) -> PredictionData:
+    """Method to load in the reports from the reports folder."""
+    try:
+        with gzip.open(file, "rb") as f:
+            data = json.load(f)
+    except FileNotFoundError as e:
+        print(f"The file {file} could not be found.")
+        raise e
+    except Exception as e:
+        print(f"An error occurred while loading the file {file}: {e}")
+        raise e
+    
+    # the data is a dictionary, we want it to be a PredictionData object
+
+    return PredictionData(**data)
+
+
+def __csv_file_to_dataframe(file: Path) -> pd.DataFrame:
     """Method to load in the reports from the reports folder."""
     report_df = pd.read_csv(file, index_col=0)
     report_df.index = pd.to_datetime(report_df.index, dayfirst=True)
@@ -21,7 +48,7 @@ def __file_to_dataframe(file: Path) -> pd.DataFrame:
 def __load_reports() -> Optional[Report]:
     """Method to load in the reports from the reports folder."""
     for report in Path("reports").glob("summary_report.csv"):
-        report = __file_to_dataframe(report)
+        report = __csv_file_to_dataframe(report)
         return report
 
 
