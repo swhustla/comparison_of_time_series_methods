338278500b62a455a14afc01e14a05bf82f3026d
ekarukes <kkarukes@gmail.com>
2023-02-10 15:27:46 +0100

    Addition to save box_plot for stock prices

--- methods/plot_results_in_box_plot.py
+++ methods/plot_results_in_box_plot.py
@@ -30,13 +30,31 @@ def plot_results_in_boxplot_from_csv(
         for chosen_metric in __chosen_metrics:
             figure_method = plot_boxplot_by_method(data_to_plot, chosen_metric)
             figure_city = plot_boxplot_by_city(data_to_plot, chosen_metric)
+            stock_company_name = data_to_plot["subset_row"][0]
             print(f"Saving {dataset_name} {chosen_metric} boxplot plot")
-        return (
-            save_plot_boxplot(
-                figure_method, dataset_name,'by_method', chosen_metric
-            ),
-            save_plot_boxplot(figure_city, dataset_name, 'by_city', chosen_metric),
-        )           
-            #save_plot_boxplot(figure_method, figure_city, dataset_name, chosen_metric)
+        if dataset_name =='Stock price' and stock_company_name == 'HD':
+            return (
+                save_plot_boxplot(
+                    figure_method, dataset_name,'by_method_young', chosen_metric
+                ),
+                save_plot_boxplot(figure_city, dataset_name, 'by_data_young', chosen_metric),
+            )           
+                #save_plot_boxplot(figure_method, figure_city, dataset_name, chosen_metric)
+        if dataset_name =='Stock price' and stock_company_name == 'GOOG':
+            return (
+                save_plot_boxplot(
+                    figure_method, dataset_name,'by_method_old', chosen_metric
+                ),
+                save_plot_boxplot(figure_city, dataset_name, 'by_data_old', chosen_metric),
+            )           
+                #save_plot_boxplot(figure_method, figure_city, dataset_name, chosen_metric)
+        else:
+             return (
+                save_plot_boxplot(
+                    figure_method, dataset_name,'by_method', chosen_metric
+                ),
+                save_plot_boxplot(figure_city, dataset_name, 'by_data', chosen_metric),
+            )           
+                #save_plot_boxplot(figure_method, figure_city, dataset_name, chosen_metric)           
 
     return draw_plot
