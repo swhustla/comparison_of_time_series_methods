fac498955af4b0937f2576f3ab60517951515618
ekarukes <kkarukes@gmail.com>
2023-02-15 10:15:11 +0100

    Introducing comparison_plot_multi into main_plot

--- main_plot.py
+++ main_plot.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python3
-
+if __name__ == "__main__":
+    from typing import (
+        Any,
+        Callable,
+        Dict,
+        List,
+        Optional,
+        Tuple,
+        Type,
+        Union,
+        Generator,
+    )
 import pandas as pd
 import numpy as np
+import logging
 from pathlib import Path
 from typing import TypeVar, List
+from predictions.Prediction import PredictionData
 
 from reports.report_loader import report_loader
+from reports.report_loader import json_report_loader
 from matplotlib import pyplot as plt
 import seaborn as sns
 import json
 import gzip
+from data.dataset import Dataset, Result
+from data.report import Report
 
 from plots.plot_results_in_heatmap import plot_results_in_heatmap_from_csv
 from plots.plot_results_in_box_plot import plot_results_in_boxplot_from_csv
+from plots.comparison_plot_multi import comparison_plot_multi
+
+
+from data.list_of_tuples import list_of_tuples
+from data.airline_passengers import airline_passengers
+from data.sun_spots import sun_spots
+from data.data_from_csv import load_from_csv
+from data.load import Load
 
 from data.india_pollution import (
     india_pollution,
@@ -20,11 +44,14 @@ from data.india_pollution import (
     get_list_of_coastal_indian_cities,
 )
 
+
 from data.stock_prices import (
     stock_prices,
     get_a_list_of_value_stock_tickers,
     get_a_list_of_growth_stock_tickers,
 )
+
+__testset_size = 0.2
 
 Data = TypeVar("Data", contravariant=True)
 Prediction = TypeVar("Prediction", covariant=True)
@@ -32,12 +59,22 @@ ConfidenceInterval = TypeVar("ConfidenceInterval", covariant=True)
 Title = TypeVar("Title", covariant=True)
 Figure = TypeVar("Figure", covariant=True)
 
+
 from methods.plot import Plot
+
+__dataset_loaders: dict[str, Load[Dataset]] = {
+    "India city pollution": india_pollution,
+    "stock_prices": stock_prices,
+    "list_of_tuples": list_of_tuples,
+    "airline_passengers": airline_passengers,
+    "sun_spots": sun_spots,
+    "csv": load_from_csv,
+}
 
 # pick one dataset from the list only
 __dataset = [
-    "Indian city pollution",
-    #   "Stock price",
+    "India city pollution",
+    # "Stock price",
     # "Airline passengers",
     # "Straight line",
     # "Sunspots",
@@ -47,22 +84,24 @@ __dataset = [
 
 # choose the subset rows for the dataset to be plotted
 __dataset_row_items: dict[str, list[str]] = {
-    "Indian city pollution": get_list_of_city_names(),  # ["Ahmedabad", "Bengaluru", "Chennai"],
-    "Stock price": get_a_list_of_value_stock_tickers(),  # ["JPM", "AAPL", "MSFT"],# get_a_list_of_growth_stock_tickers()[:2],#get_a_list_of_value_stock_tickers(),# get_a_list_of_growth_stock_tickers()[:2],#get_a_list_of_value_stock_tickers(),
+    "India city pollution": [
+        "Ahmedabad"
+    ],  # get_list_of_city_names(),  # ["Ahmedabad", "Bengaluru", "Chennai"],
+    "Stock price": get_a_list_of_growth_stock_tickers(),  # ["JPM", "AAPL", "MSFT"],# get_a_list_of_growth_stock_tickers()[:2],#get_a_list_of_value_stock_tickers(),# get_a_list_of_growth_stock_tickers()[:2],
 }
 
 
 # pick at least 2 methods from the list
 __methods = [
     "AR",
-    "linear_regression",
-    "ARIMA",
-    "HoltWinters",
-    "MA",
-    "Prophet",
+    # "linear_regression",
+    # "ARIMA",
+    # "HoltWinters",
+    # "MA",
+    # "Prophet",
     # "FCNN",
     # "FCNN_embedding",
-    "SARIMA",
+    # "SARIMA",
     # "auto_arima"
     "SES",
     # "TsetlinMachine",
@@ -71,6 +110,7 @@ __methods = [
 __plotters: dict[str, Plot[Data, Prediction, ConfidenceInterval, Title]] = {
     "heatmap": plot_results_in_heatmap_from_csv,
     "boxplot": plot_results_in_boxplot_from_csv,
+    # "comparison": comparison_plot_multi,
 }
 
 
@@ -86,9 +126,15 @@ def filter_dataframe_by_dataset_method_and_subset(
     dataframe_of_results = report_loader()
     if dataframe_of_results is None:
         return pd.DataFrame()
-    dataframe_of_results = dataframe_of_results.loc[
-        dataframe_of_results["Dataset"] == dataset
-    ]
+    if dataset == "India city pollution":
+        dataframe_of_results = dataframe_of_results.loc[
+            dataframe_of_results["Dataset"] == "Indian city pollution"
+        ]
+    else:
+        dataframe_of_results = dataframe_of_results.loc[
+            dataframe_of_results["Dataset"] == dataset
+        ]
+
     dataframe_of_results = dataframe_of_results.loc[
         dataframe_of_results["Topic"].isin(topics)
     ]
@@ -148,23 +194,53 @@ filtered_dataframe_r_squared = filtered_dataframe[
     ~filtered_dataframe.subset_row.isin(mask_low_r_squared)
 ]
 
-# decoding zip json
-filtered_dataframe_file_path_excl_cities = filtered_dataframe.loc[
-    filtered_dataframe["R2"] < -10
-].drop_duplicates(subset=["subset_row"])["Filepath"]
-print(f"cities with R2 <-10\n{filtered_dataframe_file_path_excl_cities}")
+def load_dataset(dataset_name: str) -> list[Dataset]:
+    """
+    Load the given dataset.
+    """
+    if dataset_name not in __dataset_row_items:
+        return [__dataset_loaders[dataset_name]()]
+    else:
+        return __dataset_loaders[dataset_name](__dataset_row_items[dataset_name])
 
-with gzip.open(filtered_dataframe_file_path_excl_cities[4], "rt") as zipfile:
-    my_object = json.load(zipfile)
-print(my_object.keys())
-my_object_values = my_object["values"]
-print(my_object_values)
-print(type(my_object_values))
+
+def generate_predictions_from_zip_json(data: pd.DataFrame) -> PredictionData:
+    """
+    Generates PredictionData from zip-json
+    """
+    json_data_store = []
+    for i in range(len(data["Filepath"].index)):
+        if not np.isnan(data["MAPE"][i]):
+            json_data = json_report_loader(data["Filepath"][i])
+            # json_data_store.append([data['subset_row'][i],json_data])
+            json_data_store.append(json_data)
+    return json_data
+
+
+for dataset_name in __dataset:
+    # TODO: decide if need to run on lots of datasets or just one
+    print(dataset_name)
+    data_list = load_dataset(dataset_name)
+    dataset = list(data_list)[0]
+    training_index = dataset.values.index[
+        : int(len(dataset.values.index) * (1 - __testset_size))
+    ]
+
+    for method_name in __methods:
+        #generate a comparison plot for each method
+        print(method_name)
+        comparison_plot_multi(
+            dataset.values.loc[training_index, :],
+            generate_predictions_from_zip_json(filtered_dataframe),
+        )
 
 exit()
+
 for plotter_name, plotter in __plotters.items():
     print(f"Plotting {plotter_name} for dataset: {dataset_name}")
     if plotter_name == "boxplot":
         plotter(filtered_dataframe, dataset_name)
-    else:
-        plotter(filtered_dataframe_r_squared, dataset_name)
+    # elif plotter_name == "comparison":
+    #     plotter(filtered_dataframe, dataset_name)
+    # else:
+    #     plotter(filtered_dataframe_r_squared, dataset_name)
--- plots/comparison_plot_multi.py
+++ plots/comparison_plot_multi.py
@@ -1,9 +1,10 @@
 import os
 import pandas as pd
 import numpy as np
-from typing import Tuple, Optional
+from typing import Tuple, Optional, List
 
 from data.dataset import Dataset
+from pandas import Timestamp
 
 from matplotlib import pyplot as plt
 
@@ -28,7 +29,10 @@ def __get_plot_metadata(
     prediction_data: PredictionData,
 ) -> Tuple[str, str, str]:
     """Get the metadata for the multi plot."""
-    prediction_data = prediction_data[0]
+    if isinstance(prediction_data, list):
+        # we know that if it's not a list then it has been loaded from a zip file
+        prediction_data = prediction_data[0]
+
     method_specific_plot_folder = prediction_data.plot_folder
     # get rid of the last folder
     generic_plot_folder = "/".join(method_specific_plot_folder.split("/")[:-2])
@@ -45,12 +49,68 @@ def __get_plot_metadata(
         title = prediction_data.title.split("using")[0].strip()
     else:
         title = prediction_data.title
-
     return plot_folder, plot_file_name, title
 
 
+def __convert_string_to_series(string_format: str) -> pd.Series:
+    """Convert ground truth, prediction values, in sample prediciton to correct format from string"""
+    print(f"Type {eval(string_format)}")
+
+    try:
+        tidy_series = pd.Series(eval(string_format))
+    except:
+        try:
+            print("Series conversion failed; trying to Dataframe")
+            tidy_series = pd.DataFrame(eval(string_format), index=0)
+        except:
+            print("DF conversion failed; trying to list")
+            tidy_series = list(eval(string_format))
+
+    return tidy_series
+
+
+def __tidy_up_prediction_data_object(
+    predictions_string_format: PredictionData,
+) -> List[PredictionData]:
+    cleaned_pred_data = PredictionData(
+        method_name=predictions_string_format.method_name,
+        values=predictions_string_format.values,
+        prediction_column_name=predictions_string_format.prediction_column_name,
+        ground_truth_values=predictions_string_format.ground_truth_values,
+        confidence_columns=predictions_string_format.confidence_columns,
+        title=predictions_string_format.title,
+        plot_folder=predictions_string_format.plot_folder,
+        plot_file_name=predictions_string_format.plot_file_name,
+        model_config=predictions_string_format.model_config,
+        number_of_iterations=predictions_string_format.number_of_iterations,
+        confidence_on_mean=predictions_string_format.confidence_on_mean,
+        confidence_method=predictions_string_format.confidence_method,
+        color=predictions_string_format.color,
+        in_sample_prediction=predictions_string_format.in_sample_prediction,
+    )
+
+    # get all the other values (non-series) as string
+    # for key, value in predictions_string_format:
+    #     if key not in  ["values", "ground_truth_values"]:
+    #         cleaned_pred_data[key] = value
+
+    cleaned_pred_data.ground_truth_values = __convert_string_to_series(
+        predictions_string_format.ground_truth_values
+    )
+    cleaned_pred_data.values = __convert_string_to_series(
+        predictions_string_format.values
+    )
+    cleaned_pred_data.in_sample_prediction = __convert_string_to_series(
+        predictions_string_format.in_sample_prediction
+    )
+
+    return [cleaned_pred_data]
+
+
 def __plot_full_dataset_plus_predictions(
-    training_data: pd.DataFrame, predictions: list[PredictionData], title: str
+    training_data: pd.DataFrame,
+    list_prediction_data_objects: list[PredictionData],
+    title: str,
 ) -> Figure:
     """Plot the full data and the prediction."""
     figure, axis = plt.subplots(figsize=(12, 7))
@@ -58,12 +118,18 @@ def __plot_full_dataset_plus_predictions(
     training_data_series = training_data.iloc[:, 0]
     training_data_series.plot(ax=axis, label="Training data", style=".", c="blue")
 
-    ground_truth_values = predictions[0].ground_truth_values
+    if not isinstance(list_prediction_data_objects, list):
+        # we know that if it's not a list then it has been loaded from a zip file
+        # so need to tidy it up
+        list_prediction_data_objects = __tidy_up_prediction_data_object(
+            predictions_string_format=list_prediction_data_objects
+        )
+    ground_truth_values = list_prediction_data_objects[0].ground_truth_values
+
     ground_truth_values.plot(
         ax=axis, label="Ground truth", style="x", c="blue", alpha=0.5
     )
-
-    for prediction in predictions:
+    for prediction in list_prediction_data_objects:
         prediction_series = __get_prediction_series(prediction)
         if type(prediction_series) is np.ndarray:
             prediction_series = pd.Series(
@@ -85,19 +151,30 @@ def __plot_full_dataset_plus_predictions(
             )
 
     if training_data.columns[0] == "PM2.5":
-        axis, line_1, line_2 = __add_india_who_recommendation(axis)     
-        # Create a legend 
-        second_legend = plt.legend(handles=[line_1,line_2],labels=[r"India$^*$",r"WHO$^\dagger$"],loc=1,ncol=2,title='Recommendation:')
+        axis, line_1, line_2 = __add_india_who_recommendation(axis)
+        # Create a legend
+        second_legend = plt.legend(
+            handles=[line_1, line_2],
+            labels=[r"India$^*$", r"WHO$^\dagger$"],
+            loc=1,
+            ncol=2,
+            title="Recommendation:",
+        )
         # Add the legend manually to the current Axes.
         plt.gca().add_artist(second_legend)
-        axis.annotate(r"* = Indian National Ambient Air Quality Standards, annual average PM2.5 threshold 40[$\mu g/m^3$]"+"\n"+
-                        r"$\dagger$ = World Health Organization, annual average PM2.5 threshold 5[$\mu g/m^3$]",
-            xy=(0., 0), xytext=(0, 0),
-            xycoords=('axes fraction', 'figure fraction'),
-            textcoords='offset points',
-            size=8, ha='left', va='bottom',
-            annotation_clip=False)
-
+        axis.annotate(
+            r"* = Indian National Ambient Air Quality Standards, annual average PM2.5 threshold 40[$\mu g/m^3$]"
+            + "\n"
+            + r"$\dagger$ = World Health Organization, annual average PM2.5 threshold 5[$\mu g/m^3$]",
+            xy=(0.0, 0),
+            xytext=(0, 0),
+            xycoords=("axes fraction", "figure fraction"),
+            textcoords="offset points",
+            size=8,
+            ha="left",
+            va="bottom",
+            annotation_clip=False,
+        )
 
     axis.legend(loc="upper left")
     axis.set_xlabel("Date")
@@ -110,9 +187,6 @@ def __plot_full_dataset_plus_predictions(
     axis.set_ylim(
         bottom=0, top=1.1 * max(training_data_series.max(), prediction_series.max())
     )
-
-   
-
 
     return figure
 
